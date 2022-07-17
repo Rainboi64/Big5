de9e66f6c69966cc3c576d65a49c000bdfd202a6
Rainboi64 <yaman.al.halabi1@gmail.com>
2022-07-17 18:37:27 +0300

    Cordova setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,25 @@ import "./index.css";
 import App from "./App";
 import "./config.ts";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const renderReactDom = () => {
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+if (window.cordova) {
+  document.addEventListener(
+    "deviceready",
+    () => {
+      renderReactDom();
+    },
+    false
+  );
+} else {
+  renderReactDom();
+}
